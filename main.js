a1ca46ad1e44d2af4354111ef6bf5d14383c5907
Dona <67574045+donas1319@users.noreply.github.com>
2021-03-30 23:17:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var elemt = document.querySelector("body > div > div");
 var container = document.querySelector("body > div.products");
 
-var obj;
+//Fetch from our url
 fetch("https://donas1319.github.io/JS-Assignment3/weird_products.json").then(
   function (response) {
     response.text().then(function (text) {
-      console.log(text);
-      obj = JSON.parse(text);
+      var obj = JSON.parse(text);
 
+      //loop through json
       for (let item of obj["weird-products"]) {
         let name = item["name"];
         let price = item["price"];
@@ -15,15 +15,19 @@ fetch("https://donas1319.github.io/JS-Assignment3/weird_products.json").then(
         let features = item["features"];
         let image = item["image"];
 
+        //clone the base node and create products
         var cln = elemt.cloneNode(true);
+        //Assign appropriate values
         cln.children[0].src = image;
         cln.children[1].innerText = name;
         cln.children[2].innerText = description;
         cln.children[3].innerText = features;
         cln.children[4].innerText = "$" + price;
 
+        //Append our child to document
         container.appendChild(cln);
       }
+      //delete our base node
       elemt.remove();
     });
   }
